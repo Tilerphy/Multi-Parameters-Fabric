4107144c0734e7562a94b45d7875c9aa8e688369
Zonas Sun <346410840@163.com>
2016-11-23 15:28:41 +0800

    fix bugs

--- MultiParametersJsonValueProvider.cs
+++ MultiParametersJsonValueProvider.cs
@@ -2,58 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.ApplicationModels;
-using System.Reflection;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.IO;
 using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Newtonsoft.Json.Linq;
 
 namespace MultiParametersWebApi
 {
 public class MultiParametersJsonValueProvider : IValueProvider
     {
-        private Dictionary<string, object> p = new Dictionary<string, object>();
-
+        private JContainer _container;
         public MultiParametersJsonValueProvider(ValueProviderFactoryContext context)
         {
             using (StreamReader reader = new StreamReader(context.ActionContext.HttpContext.Request.Body))
             {
-                string debug = reader.ReadToEnd();
-                JsonTextReader s = new JsonTextReader(new StringReader(debug.Trim()));
-                while (s.Read())
-                {   
-                    //the duplicate path is always useless in current context.
-                    if (p.ContainsKey(s.Path))
-                    {
-                        p[s.Path] = s.Value;
-                    }
-                    else
-                    {
-                        p.Add(s.Path, s.Value);
-                    }
+                string debug = reader.ReadToEnd().Trim();
 
-                }
+                _container = debug.StartsWith("[", StringComparison.OrdinalIgnoreCase) ?
+                    (JContainer)JArray.Parse(debug) :
+                    (JContainer)JObject.Parse(debug);
             }
         }
         public bool ContainsPrefix(string prefix)
         {
-            return p.ContainsKey(prefix);
+            JToken token = _container.SelectToken(prefix);
+            return token != null;
         }
-
         public ValueProviderResult GetValue(string key)
         {
-            if (p.ContainsKey(key))
-            {
-                ValueProviderResult s = new ValueProviderResult(new StringValues(p[key] == null ? "" : p[key].ToString()));
-                return s;
-            }
-            else
+            JToken token = _container.SelectToken(key);
+            if (token == null)
             {
                 ValueProviderResult s = new ValueProviderResult(new StringValues(string.Empty));
                 return s;
             }
-            
+            else 
+            {
+                ValueProviderResult s = new ValueProviderResult(new StringValues(token.Value<string>()));
+                return s;
+            }
         }
     }
 }
