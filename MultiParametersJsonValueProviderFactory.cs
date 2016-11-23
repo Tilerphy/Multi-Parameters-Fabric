b150e1cda0971975a692558966af05d4d0907a9a
Zonas Sun <346410840@163.com>
2016-11-23 15:29:30 +0800

    fix bugs

--- MultiParametersJsonValueProviderFactory.cs
+++ MultiParametersJsonValueProviderFactory.cs
@@ -2,12 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.ApplicationModels;
-using System.Reflection;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.IO;
 using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Newtonsoft.Json.Linq;
+
 
 namespace MultiParametersWebApi
 {
@@ -16,7 +17,7 @@ namespace MultiParametersWebApi
        public Task CreateValueProviderAsync(ValueProviderFactoryContext context)
         {
             IList<ParameterDescriptor> paramtersList = context.ActionContext.ActionDescriptor.Parameters;
-            if(paramtersList!=null && paramtersList.Count() > 0)
+            if (paramtersList != null && paramtersList.Count() > 1)
             {
                 foreach (ParameterDescriptor pd in paramtersList)
                 {
@@ -25,12 +26,18 @@ namespace MultiParametersWebApi
                         throw new NotSupportedException("Cannot use the MultiParametersJsonvalueProviderFactory when any another BindInfo exists.");
                     }
                 }
+                return Task.Run(() =>
+                {
+                    context.ValueProviders.Add(new MultiParametersJsonValueProvider(context));
+
+                });
             }
-            return Task.Run(()=> 
+            else
             {
-                context.ValueProviders.Add(new MultiParametersJsonValueProvider(context));
-                
-            });
+
+                return Task.Run(()=> { });
+            }
+            
         }
     }
 }
